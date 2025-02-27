fd77df7a2e34463301574c3788e9fc057ce85b3b
andrawicaksono <andrawicaksono14@gmail.com>
2025-02-27 16:25:21 +0700

    feat: add swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 from dotenv import load_dotenv
 
 def create_app():
@@ -14,5 +15,22 @@ def create_app():
 
     from .routes import main_blueprint
     app.register_blueprint(main_blueprint, url_prefix="/api/v1")
+
+        
+    # Swagger UI configuration
+    SWAGGER_URL = "/api/v1/swagger"
+    API_URL = "/swagger.json"
+
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={"app_name": "Smart Collateral Prediction API"}
+    )
+
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+    @app.route("/swagger.json")
+    def swagger_json():
+        return send_from_directory(".", "swagger.json")
 
     return app
